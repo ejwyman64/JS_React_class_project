b8957636ccd892ce67b05326a732d9e571defa88
Emma Wyman <ejpink4@gmail.com>
2018-09-17 19:35:45 -0400

    initial commit

---
+++ src/ToDoList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+
+class ToDoList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>ToDo1</li>
+        <li>ToDo2</li>
+      </ul>
+    );
+  }
+}
+
+export default ToDoList;
